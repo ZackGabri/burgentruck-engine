3a7ff2d184e0130e9c5d9b6aa94cd60b4aeaf015
ZackGabri <44709674+ZackGabri@users.noreply.github.com>
2025-12-12 13:16:53 +0200

    Update pattern matching style

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::collections::VecDeque;
+use std::io::{self, Write};
 
 use shakmaty::fen::Fen;
 use shakmaty::uci::UciMove;
@@ -7,32 +7,35 @@ use shakmaty::{Chess, Position};
 fn main() -> Result<(), anyhow::Error> {
     let mut pos = Chess::default();
 
-    let stdin = std::io::stdin();
+    let stdin = io::stdin();
+    let mut stdout = io::stdout();
+
     loop {
-        let mut raw_input = String::new();
-        stdin.read_line(&mut raw_input).unwrap();
+        let mut line = String::new();
+        stdin.read_line(&mut line).unwrap();
 
-        let mut args: VecDeque<&str> = raw_input.split_ascii_whitespace().collect();
+        let tokens = line.split_ascii_whitespace().collect::<Vec<_>>();
 
-        match args.pop_front().unwrap_or("") {
-            "uci" => {
+        match tokens.as_slice() {
+            ["uci", ..] => {
                 println!("id name Bürgentruck");
                 println!("id author ZackGabri & TampliteSiphronKents");
                 println!("uciok");
             }
-            "isready" => println!("readyok"),
-            "setoption" => {}
-            "ucinewgame" => {}
+            ["isready", ..] => println!("readyok"),
+            ["setoption", _args @ ..] => {}
+            ["ucinewgame", ..] => {}
 
-            "position" => match args.pop_front().unwrap_or("") {
+            ["position"] => {}
+            ["position", pos_type, args @ ..] => match *pos_type {
                 "startpos" => {
                     pos = Chess::default();
 
-                    if args.pop_front() == Some("moves") {
-                        let moves = Vec::from(args).into_iter().map(|x| x.trim());
+                    if args.first().copied() == Some("moves") {
+                        let moves = args.get(1..).unwrap_or_default();
 
                         for m in moves {
-                            let uci: UciMove = m.parse()?;
+                            let uci: UciMove = m.trim().parse()?;
                             let m = uci.to_move(&pos).unwrap();
 
                             pos.play_unchecked(m);
@@ -40,29 +43,24 @@ fn main() -> Result<(), anyhow::Error> {
                     }
                 }
                 "fen" => {
-                    let args: String = Vec::from(args).join(" ");
-                    let mut split: VecDeque<&str> = args.split("moves").collect();
+                    let args: String = args.join(" ");
+                    let split: Vec<&str> = args.split("moves").collect();
 
-                    let fen: Fen = split.pop_front().unwrap_or_default().parse()?;
+                    let fen: Fen = split.first().copied().unwrap_or_default().parse()?;
                     pos = fen.into_position(pos.castles().mode())?;
 
-                    let moves = split
-                        .pop_front()
-                        .map(|moves| moves.split_ascii_whitespace().map(|x| x.trim()));
+                    let moves = split.get(1..).unwrap_or_default();
+                    for m in moves {
+                        let uci: UciMove = m.trim().parse()?;
+                        let m = uci.to_move(&pos)?;
 
-                    if let Some(moves) = moves {
-                        for m in moves {
-                            let uci: UciMove = m.parse()?;
-                            let m = uci.to_move(&pos)?;
-
-                            pos.play_unchecked(m);
-                        }
+                        pos.play_unchecked(m);
                     }
                 }
                 _ => {}
             },
 
-            "go" => {
+            ["go", _args @ ..] => {
                 let moves = pos.legal_moves();
                 let best_move = moves.first();
 
@@ -70,9 +68,9 @@ fn main() -> Result<(), anyhow::Error> {
                     println!("bestmove {}", best_move.to_uci(pos.castles().mode()));
                 }
             }
-            "stop" => {}
+            ["stop", ..] => {}
 
-            "d" | "display" => {
+            ["d" | "display", ..] => {
                 let mut board: [[char; 8]; 8] = [['.'; 8]; 8];
                 pos.board().iter().for_each(|(square, piece)| {
                     let (file, rank) = square.coords();
@@ -94,10 +92,12 @@ fn main() -> Result<(), anyhow::Error> {
                 println!("Fen: {}", pos.board().board_fen());
             }
 
-            "" => {}
-            "quit" => break,
-            _ => println!("Unkown Command: {raw_input}"),
+            [] => {}
+            ["quit", ..] => break,
+            _ => println!("Unkown Command: {line}"),
         }
+
+        stdout.flush()?;
     }
 
     Ok(())
