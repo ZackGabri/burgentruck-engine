0b9445a6d3587c0b6c64fd6e54ecf4c69c089448
ZackGabri <44709674+ZackGabri@users.noreply.github.com>
2025-12-14 16:53:11 +0200

    Woohoo search info!

--- src/negamax.rs
+++ src/negamax.rs
@@ -1,25 +1,45 @@
 use shakmaty::{ByColor, ByRole, Chess, Color, Move, Position};
+use std::time::Instant;
 
 const DEFAULT_SEARCH_DEPTH: usize = 5;
 
 pub fn search(position: &Chess, depth: Option<usize>) -> Option<Move> {
     let depth = depth.unwrap_or(DEFAULT_SEARCH_DEPTH);
 
-    let mut best_move: Option<Move> = None;
-    let best_score = negamax(position, depth, 0, &mut best_move);
+    let mut best_line: Vec<Option<Move>> = vec![None; depth];
+    let mut node_count = 0;
 
-    dbg!(best_score);
-    dbg!(best_move)
+    let start = Instant::now();
+    let best_score = negamax(position, depth, 0, &mut best_line, &mut node_count);
+    let duration = start.elapsed();
+
+    println!(
+        "info depth {depth} score cp {} nodes {node_count} nps {} time {} pv {}",
+        best_score * 100,
+        (node_count as f64 / duration.as_secs_f64()) as usize,
+        duration.as_millis(),
+        best_line
+            .iter()
+            .filter_map(|x| x.map(|x| x.to_uci(position.castles().mode()).to_string()))
+            .collect::<Vec<String>>()
+            .join(" ")
+    );
+
+    dbg!(best_line[0])
 }
 
-pub fn negamax(position: &Chess, depth: usize, ply: usize, best_move: &mut Option<Move>) -> i32 {
+pub fn negamax(
+    position: &Chess,
+    depth: usize,
+    ply: usize,
+    best_line: &mut Vec<Option<Move>>,
+    node_count: &mut usize,
+) -> i32 {
     if depth == 0 {
         return evaluate(position);
     }
 
-    let is_root = ply == 0;
     let mut max = -69420;
-
     let moves = position.legal_moves();
 
     if moves.is_empty() {
@@ -34,14 +54,14 @@ pub fn negamax(position: &Chess, depth: usize, ply: usize, best_move: &mut Optio
 
     for mov in moves.into_iter() {
         let position = position.clone().play(mov).unwrap();
-        let score = -negamax(&position, depth - 1, ply + 1, best_move);
+        let score = -negamax(&position, depth - 1, ply + 1, best_line, node_count);
 
         if score > max {
             max = score;
-            if is_root {
-                let _ = best_move.insert(mov);
-            }
+            let _ = best_line[ply].insert(mov);
         }
+
+        *node_count += 1;
     }
 
     max
